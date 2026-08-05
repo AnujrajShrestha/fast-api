f75b9e6bc79866f6bbe168ad7140ee54fd99e68d
AnujrajShrestha <shrestharachana196@gmail.com>
2026-08-05 13:08:53 +0545

    new

--- main.py
+++ main.py
@@ -5,3 +5,11 @@ app= FastAPI()
 @app.get("/")
 def home():
     return {"message": "my frist API is working"}
+
+@app.get("/customer")
+def get_customer(customer_id: int):
+    return {
+        "customer_id": customer_id,
+        "name": "Anuj",
+        "status": "active"
+    }
